96b9d7a986e1a7ebec54a66445a5de2696de5095
gustavolugla <89364869+gustavolugla@users.noreply.github.com>
2021-09-05 19:09:23 -0300

    archivos ignorados

---
+++ adivina el numero metodo 1.py
@@ -0,0 +1,12 @@
+#metodo 1 adivina el numero
+
+import random
+n=random.randrange(1,100)
+nu=int(input('Dime el número que crees que he elegido: '))
+while nu!=n:
+    if nu>n:
+        nu=int(input('El número es mas pequeño'))
+    elif nu<n:
+        nu=int(input('El número es mas grande'))
+print('Felicidades has adivinado que el número secreto es:',n)
+
